f5336230e81b48df104d7d0ccb77a9050df55856
sakib <sakibmorshed69@gmailcom>
2025-12-13 17:06:22 +0600

    some correction added

--- src/components/Home/HomeMeals.jsx
+++ src/components/Home/HomeMeals.jsx
@@ -17,6 +17,14 @@ const HomeMeals = () => {
   if (isLoading) return <LoadingSpinner />;
   return (
     <Container>
+      <div className="text-center mt-20">
+        <h2 className="text-3xl text-center font-semibold text-red-600">
+          Comfort Food Classics
+        </h2>
+        <p className="text-xl text-gray-500 truncate my-4 text-center">
+          Eat well, live better.
+        </p>
+      </div>
       <div>
         {meals && meals.length > 0 ? (
           <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 justify-around">
